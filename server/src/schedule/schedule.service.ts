9848b1556b620775c9fe567a9c9dfeb426a67f95
Thomas Lomas <tlomas@gmail.com>
2021-02-02 00:09:50 -0500

    Fix scheduler sequencing

--- server/src/schedule/schedule.service.ts
+++ server/src/schedule/schedule.service.ts
@@ -2,8 +2,8 @@ import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
-import { concat, Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Observable, of, from } from 'rxjs';
+import { concatMap, delay, map, toArray } from 'rxjs/operators';
 import { ConfigService } from 'src/config/config.service';
 import { ItemOutputType, ItemState, PoolItem } from 'src/interfaces/PoolConfig';
 import { ItemUpdatedEvent } from 'src/item/item.event';
@@ -35,17 +35,26 @@ export class ScheduleService implements OnApplicationBootstrap {
           const job = new CronJob(schedule.time, () => {
             this.loggerService.log(`Job ${schedule.name} now running`);
 
-            const actions = schedule.actions.map(
-              (action) =>
-                this[action.action].call(
-                  this,
-                  ...action.params,
-                ) as Observable<any>,
-            );
-
-            concat(actions).subscribe(() => {
-              this.loggerService.log(`Job ${schedule.name} now completed`);
-            });
+            return from(schedule.actions)
+              .pipe(
+                concatMap(
+                  (action): Observable<any> => {
+                    this.loggerService.log(
+                      `Picked up action: ${
+                        action.action
+                      } with params: ${JSON.stringify(action.params)}`,
+                    );
+                    return this[action.action].call(
+                      this,
+                      ...action.params,
+                    ) as Observable<any>;
+                  },
+                ),
+                toArray(),
+              )
+              .subscribe(() => {
+                this.loggerService.log(`Job ${schedule.name} now completed`);
+              });
           });
 
           this.schedulerRegistry.addCronJob(schedule.name, job);
@@ -61,7 +70,16 @@ export class ScheduleService implements OnApplicationBootstrap {
   }
 
   pause(seconds: number): Observable<boolean> {
-    return of(true).pipe(delay(seconds * 1000));
+    return of(false).pipe(
+      map(() => {
+        this.loggerService.log(`Waiting ${seconds} seconds`);
+      }),
+      delay(seconds * 1000),
+      map(() => {
+        this.loggerService.log(`${seconds} has elapsed`);
+        return true;
+      }),
+    );
   }
 
   changeOutputState(
@@ -69,42 +87,43 @@ export class ScheduleService implements OnApplicationBootstrap {
     outputId: string,
     state: ItemState,
   ): Observable<any> {
-    this.loggerService.log(
-      `Changing ${itemId} -> ${outputId} state to ${state}`,
-    );
-    const config = this.configService.getConfig();
-    const items = config.items;
-    const itemIndex = items.findIndex((item) => item.id === itemId);
-    const item = items[itemIndex];
+    return new Observable((subscribe) => {
+      this.loggerService.log(
+        `Changing ${itemId} -> ${outputId} state to ${state}`,
+      );
+      const config = this.configService.getConfig();
+      const items = config.items;
+      const itemIndex = items.findIndex((item) => item.id === itemId);
+      const item = items[itemIndex];
 
-    if (item && item.active) {
-      const oldItem = JSON.parse(JSON.stringify(item)) as PoolItem;
+      if (item && item.active) {
+        const oldItem = JSON.parse(JSON.stringify(item)) as PoolItem;
 
-      if (item.outputType === ItemOutputType.SINGLE) {
-        item.outputs.forEach((output, index) => {
-          item.outputs[index].state = ItemState.OFF;
-        });
+        if (item.outputType === ItemOutputType.SINGLE) {
+          item.outputs.forEach((output, index) => {
+            item.outputs[index].state = ItemState.OFF;
+          });
+        }
+
+        const outputIndex = item.outputs.findIndex(
+          (output) => output.id === outputId,
+        );
+
+        if (outputIndex >= 0) {
+          item.outputs[outputIndex].state = state;
+        }
+
+        config.items[itemIndex] = item;
+        this.configService.updateConfig(config);
+
+        this.eventEmitter.emit(
+          `item.updated.${item.type}`,
+          new ItemUpdatedEvent(item, oldItem, item.type),
+        );
       }
 
-      const outputIndex = item.outputs.findIndex(
-        (output) => output.id === outputId,
-      );
-
-      if (outputIndex >= 0) {
-        item.outputs[outputIndex].state = state;
-      }
-
-      console.log(item);
-
-      config.items[itemIndex] = item;
-      this.configService.updateConfig(config);
-
-      this.eventEmitter.emit(
-        `item.updated.${item.type}`,
-        new ItemUpdatedEvent(item, oldItem, item.type),
-      );
-    }
-
-    return of(true);
+      subscribe.next(true);
+      subscribe.complete();
+    });
   }
 }
